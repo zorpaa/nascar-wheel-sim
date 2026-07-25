e6bcf8fbdd22b9751819a9967078b8689decd8de
zorpaa <alexanderbtew2@gmail.com>
2026-07-25 02:16:48 -0700

    Refactor tracks object for cleaner formatting

--- data/tracks.js
+++ data/tracks.js
@@ -1,31 +1,9 @@
 const tracks = {
-
-    Daytona:{
-        type:"Superspeedway"
-    },
-
-    Talladega:{
-        type:"Superspeedway"
-    },
-
-    Bristol:{
-        type:"Short Track"
-    },
-
-    Martinsville:{
-        type:"Short Track"
-    },
-
-    Sonoma:{
-        type:"Road Course"
-    },
-
-    WatkinsGlen:{
-        type:"Road Course"
-    },
-
-    Charlotte:{
-        type:"Intermediate"
-    }
-
+    Daytona:{type:"Superspeedway"},
+    Talladega:{type:"Superspeedway"},
+    Bristol:{type:"Short Track"},
+    Martinsville:{type:"Short Track"},
+    Sonoma:{type:"Road Course"},
+    WatkinsGlen:{type:"Road Course"},
+    Charlotte:{type:"Intermediate"}
 };
