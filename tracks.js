c534fadc0e29ae2a054348a51fc1255bf4fa49b5
zorpaa <alexanderbtew2@gmail.com>
2026-07-25 02:12:45 -0700

    Add tracks.js with track types

---
+++ tracks.js
@@ -0,0 +1,31 @@
+const tracks = {
+
+    Daytona:{
+        type:"Superspeedway"
+    },
+
+    Talladega:{
+        type:"Superspeedway"
+    },
+
+    Bristol:{
+        type:"Short Track"
+    },
+
+    Martinsville:{
+        type:"Short Track"
+    },
+
+    Sonoma:{
+        type:"Road Course"
+    },
+
+    WatkinsGlen:{
+        type:"Road Course"
+    },
+
+    Charlotte:{
+        type:"Intermediate"
+    }
+
+};
