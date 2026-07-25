cd539ce0980b0d7fef9f2914941c12b92bef5cfd
zorpaa <alexanderbtew2@gmail.com>
2026-07-24 19:39:41 -0700

    Replace 'speed' with 'weight' for drivers

--- cup.js
+++ cup.js
@@ -3,31 +3,31 @@ const cupDrivers = [
         name: "Kyle Larson",
         team: "Hendrick Motorsports",
         chartered: true,
-        speed: 0.5
+        weight: 0.5
     },
     {
         name: "William Byron",
         team: "Hendrick Motorsports",
         chartered: true,
-        speed: 0.8
+        weight: 0.8
     },
     {
         name: "Ryan Blaney",
         team: "Team Penske",
         chartered: true,
-        speed: 1.2
+        weight: 1.2
     },
     {
         name: "Cody Ware",
         team: "Rick Ware Racing",
         chartered: true,
-        speed: 5
+        weight: 5
     },
     {
         name: "BJ McLeod",
         team: "Live Fast Motorsports",
         chartered: false,
-        speed: 4,
+        weight: 4,
         entryWeight: 5
     }
 ];
