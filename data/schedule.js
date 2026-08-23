77a570915270b30adc57d959f0e6478d1f62c26c
zorpaa <alexanderbtew2@gmail.com>
2026-08-22 20:14:07 -0700

    Add schedule data for Cup, O'Reilly, and Truck races

---
+++ data/schedule.js
@@ -0,0 +1,21 @@
+const schedule = {
+    Cup:[
+        {race:1,name:"Daytona 500",track:"Daytona"},
+        {race:2,name:"Atlanta",track:"Atlanta"},
+        {race:3,name:"Las Vegas",track:"Las Vegas"},
+        {race:4,name:"Phoenix",track:"Phoenix"},
+        {race:5,name:"Bristol",track:"Bristol"}
+    ],
+
+    "O'Reilly":[
+        {race:1,name:"Daytona",track:"Daytona"},
+        {race:2,name:"Atlanta",track:"Atlanta"},
+        {race:3,name:"Las Vegas",track:"Las Vegas"}
+    ],
+
+    Truck:[
+        {race:1,name:"Daytona",track:"Daytona"},
+        {race:2,name:"Atlanta",track:"Atlanta"},
+        {race:3,name:"Bristol",track:"Bristol"}
+    ]
+};
