ea015f03bb8d6617f9667e986dbd3603246d1717
zorpaa <alexanderbtew2@gmail.com>
2026-08-22 21:30:25 -0700

    Add finish types to finishTypes.js

--- finishTypes.js
+++ finishTypes.js
@@ -1,3 +1,11 @@
 const finishTypes = [
-
+    {name:"Side-by-side",weight:...},
+    {name:"Close at the End",weight:...},
+    {name:"Comeback Win",weight:...},
+    {name:"Nearly Passed at the End",weight:...},
+    {name:"3-Wide Photo Finish",weight:...},
+    {name:"Fuel Mileage",weight:...},
+    {name:"Dominance",weight:...},
+    {name:"4-Wide Photo Finish",weight:...},
+    {name:"2-Wide Photo Finish",weight:...}
 ];
