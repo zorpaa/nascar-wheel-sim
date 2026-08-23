90d91c6f6b7632e8e9614b07015199e5accb9241
zorpaa <alexanderbtew2@gmail.com>
2026-08-22 20:44:05 -0700

    Add new tracks and categorize existing ones

--- data/tracks.js
+++ data/tracks.js
@@ -1,9 +1,41 @@
 const tracks = {
+
+    // Superspeedways
     Daytona:{type:"Superspeedway"},
     Talladega:{type:"Superspeedway"},
+    Atlanta:{type:"Superspeedway"},
+    Indianapolis:{type:"Superspeedway"},
+    Michigan:{type:"Superspeedway"},
+
+    // Intermediate
+    LasVegas:{type:"Intermediate"},
+    Charlotte:{type:"Intermediate"},
+    Kansas:{type:"Intermediate"},
+    Texas:{type:"Intermediate"},
+    Homestead:{type:"Intermediate"},
+    Michigan:{type:"Intermediate"},
+    Pocono:{type:"Intermediate"},
+    Nashville:{type:"Intermediate"},
+    Chicagoland:{type:"Intermediate"},
+    Gateway:{type:"Intermediate"},
+    Phoenix:{type:"Intermediate"},
+    
+    // Short Tracks
     Bristol:{type:"Short Track"},
     Martinsville:{type:"Short Track"},
+    Richmond:{type:"Short Track"},
+    Iowa:{type:"Short Track"},
+    Darlington:{type:"Short Track"},
+    Dover:{type:"Short Track"},
+    NewHampshire:{type:"Short Track"},
+    California:{type:"Short Track"},
+
+    // Road Courses
     Sonoma:{type:"Road Course"},
     WatkinsGlen:{type:"Road Course"},
-    Charlotte:{type:"Intermediate"}
+    COTA:{type:"Road Course"},
+    Chicago:{type:"Street Course"},
+    CharlotteRoval:{type:"Street Course"},
+    RoadAmerica:{type:"Street Course"},
+    CanadianTire:{type:"Street Course"}
 };
