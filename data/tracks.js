8c5b7e5874591d901f96074be916e346ab86abdd
zorpaa <alexanderbtew2@gmail.com>
2026-08-22 20:52:53 -0700

    Update tracks.js

--- data/tracks.js
+++ data/tracks.js
@@ -1,41 +1,132 @@
 const tracks = {
 
     // Superspeedways
-    Daytona:{type:"Superspeedway"},
-    Talladega:{type:"Superspeedway"},
-    Atlanta:{type:"Superspeedway"},
-    Indianapolis:{type:"Superspeedway"},
-    Michigan:{type:"Superspeedway"},
+    Daytona:{
+        name:"Daytona International Speedway",
+        type:"Superspeedway"
+    },
+    Talladega:{
+        name:"Talladega Superspeedway",
+        type:"Superspeedway"
+    },
+    Atlanta:{
+        name:"Atlanta Motor Speedway",
+        type:"Superspeedway"
+    },
+    Indianapolis:{
+        name:"Indianapolis Motor Speedway",
+        type:"Superspeedway"
+    },
+    Michigan:{
+        name:"Michigan International Speedway",
+        type:"Superspeedway"
+    },
 
     // Intermediate
-    LasVegas:{type:"Intermediate"},
-    Charlotte:{type:"Intermediate"},
-    Kansas:{type:"Intermediate"},
-    Texas:{type:"Intermediate"},
-    Homestead:{type:"Intermediate"},
-    Michigan:{type:"Intermediate"},
-    Pocono:{type:"Intermediate"},
-    Nashville:{type:"Intermediate"},
-    Chicagoland:{type:"Intermediate"},
-    Gateway:{type:"Intermediate"},
-    Phoenix:{type:"Intermediate"},
-    
+    LasVegas:{
+        name:"Las Vegas Motor Speedway",
+        type:"Intermediate"
+    },
+    Charlotte:{
+        name:"Charlotte Motor Speedway",
+        type:"Intermediate"
+    },
+    Kansas:{
+        name:"Kansas Speedway",
+        type:"Intermediate"
+    },
+    Texas:{
+        name:"Texas Motor Speedway",
+        type:"Intermediate"
+    },
+    Homestead:{
+        name:"Homestead-Miami Speedway",
+        type:"Intermediate"
+    },
+    Pocono:{
+        name:"Pocono Raceway",
+        type:"Intermediate"
+    },
+    Nashville:{
+        name:"Nashville Superspeedway",
+        type:"Intermediate"
+    },
+    Chicagoland:{
+        name:"Chicagoland Speedway",
+        type:"Intermediate"
+    },
+    Gateway:{
+        name:"World Wide Technology Raceway",
+        type:"Intermediate"
+    },
+    Phoenix:{
+        name:"Phoenix Raceway",
+        type:"Intermediate"
+    },
+
     // Short Tracks
-    Bristol:{type:"Short Track"},
-    Martinsville:{type:"Short Track"},
-    Richmond:{type:"Short Track"},
-    Iowa:{type:"Short Track"},
-    Darlington:{type:"Short Track"},
-    Dover:{type:"Short Track"},
-    NewHampshire:{type:"Short Track"},
-    California:{type:"Short Track"},
+    Bristol:{
+        name:"Bristol Motor Speedway",
+        type:"Short Track"
+    },
+    Martinsville:{
+        name:"Martinsville Speedway",
+        type:"Short Track"
+    },
+    Richmond:{
+        name:"Richmond Raceway",
+        type:"Short Track"
+    },
+    Iowa:{
+        name:"Iowa Speedway",
+        type:"Short Track"
+    },
+    Darlington:{
+        name:"Darlington Raceway",
+        type:"Short Track"
+    },
+    Dover:{
+        name:"Dover Motor Speedway",
+        type:"Short Track"
+    },
+    NewHampshire:{
+        name:"New Hampshire Motor Speedway",
+        type:"Short Track"
+    },
+    California:{
+        name:"Auto Club Speedway",
+        type:"Short Track"
+    },
 
     // Road Courses
-    Sonoma:{type:"Road Course"},
-    WatkinsGlen:{type:"Road Course"},
-    COTA:{type:"Road Course"},
-    Chicago:{type:"Street Course"},
-    CharlotteRoval:{type:"Street Course"},
-    RoadAmerica:{type:"Street Course"},
-    CanadianTire:{type:"Street Course"}
+    Sonoma:{
+        name:"Sonoma Raceway",
+        type:"Road Course"
+    },
+    WatkinsGlen:{
+        name:"Watkins Glen International",
+        type:"Road Course"
+    },
+    COTA:{
+        name:"Circuit of the Americas",
+        type:"Road Course"
+    },
+
+    // Street Courses
+    Chicago:{
+        name:"Chicago Street Course",
+        type:"Street Course"
+    },
+    CharlotteRoval:{
+        name:"Charlotte Motor Speedway Roval",
+        type:"Street Course"
+    },
+    RoadAmerica:{
+        name:"Road America",
+        type:"Road Course"
+    },
+    CanadianTire:{
+        name:"Canadian Tire Motorsport Park",
+        type:"Road Course"
+    }
 };
